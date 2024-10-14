6458f5b1ef00e8c8ccce07700ff94a6d55293717
gonzaotc <gonza.otc@gmail.com>
2024-10-14 18:57:54 -0300

    Covered more testing cases

--- test/utils/utils.ts
+++ test/utils/utils.ts
@@ -1,4 +1,4 @@
-import { Connection } from '@solana/web3.js';
+import { Connection, Keypair, Transaction } from '@solana/web3.js';
 
 export async function getCluster(connection: Connection): Promise<string> {
   // Get the genesis hash
@@ -16,4 +16,20 @@ export async function getCluster(connection: Connection): Promise<string> {
       // If it doesn't match any known cluster, it's likely localhost
       return 'localhost';
   }
+}
+
+
+export async function estimateTransactionFee(connection: Connection, transaction: Transaction, signers: Keypair[]): Promise<number> {
+  const { blockhash, lastValidBlockHeight } = await connection.getLatestBlockhash();
+  transaction.recentBlockhash = blockhash;
+  transaction.sign(...signers);
+
+  const message = transaction.compileMessage();
+  const fees = await connection.getFeeForMessage(message);
+
+  if (fees === null) {
+    throw new Error('Failed to estimate fee');
+  }
+
+  return fees.value;
 }
