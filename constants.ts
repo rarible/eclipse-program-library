37aa3d3710947e8041bcdf8221adcbebd1ba0c74
gonzaotc <gonza.otc@gmail.com>
2024-10-15 11:57:30 -0300

    Added instruction to run tests

--- constants.ts
+++ constants.ts
@@ -1,6 +1,4 @@
-export const EDITIONS_PROGRAM_ID =
-  'GHVmd43GebD2SSedqG1AAh76izf5CPAhz6KotkZyRzUD';
-export const EDITIONS_CONTROLS_PROGRAM_ID =
-  'CCWfNBFcrjbKSjKes9DqgiQYsnmCFgCVKx21mmV2xWgN';
-export const TOKEN_GROUP_EXTENSION_PROGRAM_ID =
-  '5hx15GaPPqsYA61v6QpcGPpo125v7rfvEfZQ4dJErG5V';
+// @dev: sync with Anchor.toml to ensure correct program ids are used on tests
+export const EDITIONS_PROGRAM_ID = 'GHVmd43GebD2SSedqG1AAh76izf5CPAhz6KotkZyRzUD';
+export const EDITIONS_CONTROLS_PROGRAM_ID = 'CCWfNBFcrjbKSjKes9DqgiQYsnmCFgCVKx21mmV2xWgN';
+export const TOKEN_GROUP_EXTENSION_PROGRAM_ID = '5hx15GaPPqsYA61v6QpcGPpo125v7rfvEfZQ4dJErG5V';
